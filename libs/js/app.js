7bd8ba6ca6c6e5b0a5477dddecfb5e1543732988
Cesar Otero <cesar.otero@hunabsys.com>
2016-02-13 12:44:01 -0700

    Update until module 4

---
+++ libs/js/app.js
@@ -0,0 +1,67 @@
+(function(){
+	var app = angular.module('store',[ ]);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	app.controller("PanelController", function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: '. . .',
+			canPurchase: true,
+			reviews:[
+			{
+				stars: 5,
+				body: "Love the product!",
+				author: "abcd@abcd.com",
+			},
+			{
+				stars: 5,
+				body: "Love the product!",
+				author: "efgh@efgh.com",
+			}
+			]
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: '. . .',
+			canPurchase: true,
+			reviews:[
+			{
+				stars: 2,
+				body: "Love the product!",
+				author: "abcd@abcd.com",
+			},
+			{
+				stars: 4,
+				body: "Love the product!",
+				author: "efgh@efgh.com",
+			}
+			]
+		}
+	];
+})();
